44fa701f4d87d2ec8fd0415c62f5ce290ada8b17
Vinh <ducvinhnguyen0201@gmail.com>
2020-11-25 21:21:39 +0700

    done practice

--- practice-23-11-2020/src/findArrayElement.java
+++ practice-23-11-2020/src/findArrayElement.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class findArrayElement {
+import java.util.Scanner;
+
+public class findArrayElement {
+    public static void main(String[] args) {
+        String[] studens = {"Chris", "Micheal", "Camila", "Sienna", "Tanya", "Connor", "Zac", "Mallory", "Zoe", "Emily"};
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a name's student:");
+        String input_name = scanner.nextLine();
+
+        boolean isExist = false;
+        for (int i = 0; i < studens.length; i++) {
+            if(studens[i].equals(input_name)){
+                System.out.println("Position of the student in the list " + input_name + " is: " + (i + 1));
+                isExist = true;
+                break;
+            }
+        }
+        if(!isExist)
+            System.out.println("Not found" + input_name + "in the list.");
+    }
 }
--- practice-23-11-2020/src/temperatureExchanger.java
+++ practice-23-11-2020/src/temperatureExchanger.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class temperatureExchanger {
+import java.util.Scanner;
+
+public class temperatureExchanger {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double tempF;
+        double tempC;
+        int choice;
+
+        do {
+            System.out.println("Menu.");
+            System.out.println("1. Fahrenheit to Celsius");
+            System.out.println("2. Celsius to Fahrenheit");
+            System.out.println("0. Exit");
+            System.out.println("Enter your choice: ");
+            choice = input.nextInt();
+
+            switch (choice){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 0:
+                    System.exit(0);
+            }
+        } while (choice != 0);
+    }
+    public static double ctoF(double tempC){
+        double tempF = (9.0 / 5) * tempC + 32;
+        return tempF;
+    }
+    public static double ftoC(double tempF){
+        double tempC  = (5.0 / 9) * (tempF - 32);
+        return tempC;
+    }
 }
